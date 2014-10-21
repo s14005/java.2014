b869b0ed4ef87999ff698bb985267a6825fcc9d5
s14005 <s14005@std.it-college.ac.jp>
2014-10-21 12:43:35 +0900

    java.2014

--- Ex2.java
+++ Ex2.java
@@ -48,7 +48,55 @@ class Ex2_2 extends Ex{
                   System.out.println("zの値は" + z + "です。");
                   System.out.println("合計は" + (x + y + z) + "です。");
                   System.out.println("平均は" + (x + y + z) / 2 + "です。");          }
-         }
+         }      
+ class Ex2_3 extends Ex{                                                  
+   void execute(){
+   Scanner stdIn = new Scanner(System.in);
+
+         System.out.print("整数値:");
+         int a = stdIn.nextInt();
+        
+         System.out.println(a);
+          }
+          }
+class Ex2_4 extends Ex{
+void execute(){
+Scanner stdIn = new Scanner(System.in);
+
+System.out.print("整数値:");
+int a = stdIn.nextInt();
+
+System.out.println(a - 10);
+System.out.println(a + 10);
+}
+}
+class Ex2_5 extends Ex{
+void execute(){
+Scanner stdIn = new Scanner(System.in);
+
+System.out.println("xの値");
+int x = stdIn.nextInt();
+
+System.out.println("yの値");
+int y = stdIn.nextInt();
+
+System.out.println((x + y) + "和");
+System.out.println((x + y)/2 + "平均");
+}
+}
+class Ex2_6 extends Ex{
+void execute(){
+Scanner stdIn = new Scanner(System.in);
+
+System.out.println("底辺");
+double x = stdIn.nextDouble();
+
+System.out.println("高さ");
+double y = stdIn.nextDouble();
+
+System.out.println((x*y/2)+"三角形の面積");
+}
+}
         public class Ex2 {
 	public static void main(String[] args){
 		Ex a;
@@ -58,5 +106,13 @@ class Ex2_2 extends Ex{
                 a.execute();
                 a = new Ex2_2();
                 a.execute();
+                a = new Ex2_3();
+                a.execute();
+                a = new Ex2_4();
+                a.execute();
+                a = new Ex2_5();
+                a.execute();
+                a = new Ex2_6();
+                a.execute();
 	}
-}
+
