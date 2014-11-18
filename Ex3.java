f55388aba44afe70c39e0d936ce5f4911c987e1c
s14005 <s14005@std.it-college.ac.jp>
2014-11-18 09:41:48 +0900

    Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -1,39 +1,62 @@
 class Ex3{
 
 	public String ex3_0(int n){
-
-		return "dummy";
+        if(0<n){
+        return "その値は正です";
+        }else{
+        return "その値は０か負です";
+        }
 
 	}
 
 	public int ex3_1(int n){
 		
-		return -1;
+		return Math.abs(n);
 
 	}
 
 	public String ex3_2(int a, int b){
-
-		return "dummy";
-	}
-
+        if(a%b == 0){
+        return b + "は" + a + "の約数です";
+        }else{
+        return b + "は" + a + "の約数ではありません";
+        }
+        }
 	public String ex3_3(int a, int b){
+        if(b<a){
+        return a + "の方が大きいです";
+        }else if(a<b){
+        return b + "の方が大きいです";
+        }else{
+        return "同じ値です";
+        }
 
-		return "dummy";
-
-	}
+        }
 
 	public String ex3_4(int a){
-
-		return "dummy";
-
-	}
-
+        if(a/5 >= 0){
+        return "その値は５で割り切れます";
+        }else if(a/5 == 0){
+        return "その値は割り切れません";
+        }else{
+        return "正でない整数値です";
+        }
+        }
 	public String ex3_5(int a){
+        if(a%10 == 0 ){
+        return "その値は10の倍数です";
+        }else{
+        return "その値は10の倍数ではありません";
+        }
+	if(a<0){
+        return "正でない整数値です";
+        }else{
+        return "それ以外"; 
+        }
+        }
 
-		return "dummy";
-	}
-	
+        
+        
 	public String ex3_6(int a){
 		return "dummy";
 	}
